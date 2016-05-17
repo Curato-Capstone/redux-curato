2e181bb05a73cf008e5fb90f31fb5f4cd8919798
Alex <alex_ngo@live.com>
2016-05-16 23:41:11 -0700

    fixed places export

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import userReducer, * as user from './modules/user';
 import suggestionsReducer, * as suggestions from './modules/suggestions';
-import placeReducer, * as places from './modules/suggestions';
+import placesReducer, * as places from './modules/places';
 import authReducer, * as auth from './modules/auth';
 import globalReducer, * as global from './modules/global';
 
 export {
     userReducer,
     suggestionsReducer,
-    placeReducer,
+    placesReducer,
     authReducer,
     globalReducer
 };
